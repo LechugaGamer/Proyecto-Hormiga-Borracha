956190eefb8bfc6f7b0dc1aec815e29b39d4758c
unknown <juanmaboniarce@gmail.com>
2024-10-27 23:18:29 -0600

    Modelo del fitness

--- algoritmo genético.py
+++ algoritmo genético.py
@@ -53,18 +53,25 @@ a: alcohol
 
 #algoritmo main:
 
+#la población se guarda en puntajes-hormiga
+
 Alelos = {"arriba":  [-1,0], 
           "abajo":  [1,0],
           "derecha": [0,1],
           "izquierda": [1,0],
-          "comer": "comer"}
-alelos_keys = list(Alelos.keys())
+          "comer arriba": "comer",
+          "comer abajo": "comer",
+          "comer derecha": "comer",
+          "comer izquierda": "comer"}
+
+alelos_keys = list(Alelos.keys()) #para llamar a los alelos
 
 Puntos_general = {"e": 100,
           " " : 10,
           "a" : -20,
           "s" : 30,
           "p" : "muerte" #la mata, la elimina directamente, se cruzan las dos mejores
+          #si no hay dos mejores se genera una nueva
 }
 
 
@@ -82,13 +89,15 @@ def encontrar_pos(matriz, elemento):
             
 start = encontrar_pos(laberinto, "s")
 exit = encontrar_pos(laberinto, "e")
+
 def action(al):
     return Alelos[al]
 
-def adn_i (): 
+def adn_i ():  
     return [random.choice(alelos_keys)] #retorna una lista con un alelo incial
 
 def mutacion(adn):
+
     for alelo in adn:
         muta = random.randint(0,10)
 
@@ -119,5 +128,43 @@ Otro va a ser la cantidad de puntos de alcohol, entre menos mejor.
 Y si come veneno directamente se muere XD.
 """
 
+def comer(dirección, matriz, x, y):
+    if dirección == "comer arriba":
+        pass
 
-print(adn_i())
+
+
+
+    
+
+def fitness(matriz, adn):
+    global start, exit
+
+    xi = start[0]
+    yi = start[1]
+
+    xf= exit[0]
+    yf= exit[1]
+    
+    x_actual = xi
+    y_actual = xi
+
+    puntos_azucar = 0
+    puntos_alcohol = 0
+
+    distancia_rel = 0
+
+    for alelo in adn:
+
+        if ( 0 <= Alelos[alelo][0] + x_actual < len(matriz[0]) and 0 <= Alelos[alelo][1] + y_actual < len(matriz) 
+            ):
+
+            if Alelos[alelo] != "comer":
+
+           
+                x_actual += Alelos[alelo][0]
+                y_actual += Alelos[alelo][0] 
+
+            if Alelos[alelo] == "comer":
+
+                objeto = comer(alelo, matriz, x_actual, y_actual)
